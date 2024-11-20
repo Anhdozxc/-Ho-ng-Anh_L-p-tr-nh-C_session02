8ec430354aa35e398cceaa5cffe009299be8b4a3
Đỗ Hoàng Anh <dohoanganh.russia@gmail.com>
2024-11-21 04:23:52 +0700

    Upload bài tập Session02

---
+++ Session02_2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>	
+
+int main() {
+  int firstNumber = 15 ;
+  int secondNumber = 2 ;
+  int tong=firstNumber+secondNumber ;
+  printf( "Tong hai so la %d ", tong );
+	return 0 ;
+}
+
+
---
+++ Session02_5.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>	
+
+int main () {
+	
+	int firstEdge = 15 ; 
+	int secondEdge = 8 ; 
+
+	int tong=firstEdge+secondEdge;
+	int perimeter=tong*2;
+	printf("Chu vi hinh chu nhat la %d ", perimeter);	
+	
+	int acreage=firstEdge*secondEdge ;
+	printf("Dien tich hinh chu nhat la %d", acreage);
+
+	return 0 ;
+}
+
